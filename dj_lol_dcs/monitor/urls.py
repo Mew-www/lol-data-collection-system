da8f26932123e7cf77e930de386a2c0bc4d43b97
Mew-www <mew.proxy@hotmail.com>
2018-01-26 11:04:17 +0200

    Prettify url routing, prepare paths for gathering activity & raw data summary

--- dj_lol_dcs/monitor/urls.py
+++ dj_lol_dcs/monitor/urls.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 from .views.ratelimit import ratelimit_endpoints, ratelimit_quota_graph
 
 
+# /monitor
 urlpatterns = [
     re_path(r'^/?$', lambda request: render(request, 'monitor.html')),
-    re_path(r'^/ratelimit_endpoints$', ratelimit_endpoints),
-    re_path(r'^/(?P<ratelimit_endpoint>\w+)/ratelimit_quota.png', ratelimit_quota_graph),
+    re_path(r'^/ratelimit/endpoints$', ratelimit_endpoints),
+    re_path(r'^/ratelimit/(?P<ratelimit_endpoint>\w+)/quota.png$', ratelimit_quota_graph),
+    re_path(r'^/gathering/activity$', lambda request: render(request, 'monitor.html')),  # Placeholder
+    re_path(r'^/gathering/data/summary$', lambda request: render(request, 'monitor.html'))  # Placeholder
 ]
