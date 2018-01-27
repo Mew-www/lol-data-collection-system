39cfb703fd03b9fe8646f1de1f59d506566ed4be
Mew-www <mew.proxy@hotmail.com>
2018-01-27 20:07:20 +0200

    Reformat matches summary to per region

--- dj_lol_dcs/monitor/views/gathering.py
+++ dj_lol_dcs/monitor/views/gathering.py
@@ -24,17 +24,22 @@ def gatherers_activity_timestamps(request):
 def gathered_data_summary(request):
     """Returns common data summary information. As JSON object {games_total: .., etc}"""
 
-    all_saved_matches = HistoricalMatch.objects.all()
+    all_matches = HistoricalMatch.objects.all()
+    spanned_regions = list(all_matches.values_list('region__name', flat=True).distinct())
+    matches_per_region = {
+        r: {
+            'total': int(all_matches.filter(region__name=r).count()),
+            'master': int(all_matches.filter(region__name=r).filter(regional_tier_avg__contains='MASTER').count()),
+            'diamond': int(all_matches.filter(region__name=r).filter(regional_tier_avg__contains='DIAMOND').count()),
+            'platinum': int(all_matches.filter(region__name=r).filter(regional_tier_avg__contains='PLATINUM').count()),
+            'gold': int(all_matches.filter(region__name=r).filter(regional_tier_avg__contains='GOLD').count()),
+            'silver': int(all_matches.filter(region__name=r).filter(regional_tier_avg__contains='SILVER').count()),
+            'bronze': int(all_matches.filter(region__name=r).filter(regional_tier_avg__contains='BRONZE').count())
+        } for
+        r in
+        spanned_regions}
 
     return HttpResponse(json.dumps({
-        'matches': {
-            'regions': list(all_saved_matches.values_list('region__name', flat=True).distinct()),
-            'total': int(all_saved_matches.count()),
-            'master': int(all_saved_matches.filter(regional_tier_avg__contains='MASTER').count()),
-            'diamond': int(all_saved_matches.filter(regional_tier_avg__contains='DIAMOND').count()),
-            'platinum': int(all_saved_matches.filter(regional_tier_avg__contains='PLATINUM').count()),
-            'gold': int(all_saved_matches.filter(regional_tier_avg__contains='GOLD').count()),
-            'silver': int(all_saved_matches.filter(regional_tier_avg__contains='SILVER').count()),
-            'bronze': int(all_saved_matches.filter(regional_tier_avg__contains='BRONZE').count())
-        }
+        'matches_total': int(all_matches.count()),
+        'matches_per_region': matches_per_region,
     }))
