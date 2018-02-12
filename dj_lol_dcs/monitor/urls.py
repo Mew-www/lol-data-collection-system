a929982eea0d46d65090428b8abb1229c5154653
Mew-www <mew.proxy@hotmail.com>
2018-02-12 17:48:42 +0200

    Add (compressed) db dump snapshotting interface

--- dj_lol_dcs/monitor/urls.py
+++ dj_lol_dcs/monitor/urls.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 from django.shortcuts import render
 from .views.ratelimit import ratelimit_endpoints, ratelimit_quota_graph
 from .views.gathering import gatherers_activity_timestamps, gathered_data_summary
+from .views.snapshot import generate_database_dump
 
 
 # /monitor
@@ -10,5 +11,6 @@ urlpatterns = [
     re_path(r'^/ratelimit/endpoints$', ratelimit_endpoints),
     re_path(r'^/ratelimit/(?P<ratelimit_endpoint>\w+)/quota.png$', ratelimit_quota_graph),
     re_path(r'^/gathering/activity$', gatherers_activity_timestamps),
-    re_path(r'^/gathering/data/summary$', gathered_data_summary)
+    re_path(r'^/gathering/data/summary$', gathered_data_summary),
+    re_path(r'^/snapshot/lol_dcs_db.sql.zip$', generate_database_dump)
 ]
---
+++ dj_lol_dcs/monitor/views/snapshot.py
@@ -0,0 +1,40 @@
+from django.http import FileResponse
+from django.conf import settings
+
+import pexpect
+import os
+import time
+
+
+def generate_database_dump(request):
+
+    # Remove any existing database dumps older than 10min
+    tmp_folder = os.path.join(settings.BASE_DIR, 'tmp')
+    for filename in os.listdir(tmp_folder):
+        last_modified = os.path.join(tmp_folder, filename)
+        if last_modified < (time.time() - 60*10):
+            os.remove(os.path.join(tmp_folder, filename))
+
+    # Make sure not to override ongoing (another) request from before (within 10min)
+    tmp_file_location = os.path.join(tmp_folder, 'dcs_dump.sql.zip')
+    variant = 1
+    exists = os.path.exists(tmp_file_location)
+    while exists:
+        tmp_file_location = os.path.join(settings.BASE_DIR, 'tmp', 'dcs_dump_{}.sql.zip'.format(variant))
+        exists = os.path.exists(tmp_file_location)
+        variant += 1
+
+    # Dump database
+    child_process = pexpect.spawn('pg_dump', ['--username={}'.format(os.environ['DJ_PG_USERNAME']),
+                                              '--host=localhost',
+                                              '--password',
+                                              '--clean',
+                                              '--create',
+                                              '--format=c',
+                                              '--file={}'.format(tmp_file_location),
+                                              os.environ['DJ_PG_DBNAME']])
+    child_process.expect('Password:')
+    child_process.sendline(os.environ['DJ_PG_PASSWORD'])
+    child_process.wait()  # Wait for pg_dump process to finish; Blocking
+
+    return FileResponse(open(tmp_file_location, 'rb'), streaming_content='application/octet-stream')
